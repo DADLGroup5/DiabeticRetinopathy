32ca4b171444855f4a0a4d68e7140af4bd9f4367
Saurabh Sinha <51476953+PrinceSs6312@users.noreply.github.com>
2020-01-02 15:05:30 +0800

    Update Diabetic_Retinopathy_Image_Labelling.py

--- Diabetic_Retinopathy_Image_Labelling.py
+++ Diabetic_Retinopathy_Image_Labelling.py
@@ -4,7 +4,7 @@ def main():
    #setting path location
    path="C:/MyProject/"
    for filename in os.listdir(path):
-      my_dest ="moderate-" + str(i) + ".tiff"
+      my_dest ="mild-" + str(i) + ".tiff"
       my_source =path + filename
       my_dest =path + my_dest
       #rename() function will rename all the files
@@ -13,8 +13,3 @@ def main():
 if __name__ == '__main__':
    # Calling main() function
    main()
-   
-   
-  
-  
-  
